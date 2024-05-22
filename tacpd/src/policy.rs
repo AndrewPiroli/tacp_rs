eb7d42f6c2242c4a4618659ecb2a49c5dd16f177
Andrew Piroli <drewpiroli@gmail.com>
2024-05-22 10:12:55 -0400

    Finish yaml switch

--- tacpd/src/policy.rs
+++ tacpd/src/policy.rs
@@ -1,6 +1,5 @@
 use std::{error::Error, net::IpAddr};
-use toml::Table;
-use yaml::StrictYamlLoader;
+use yaml::{StrictYaml, StrictYamlLoader};
 use fnv::FnvHashMap;
 use crate::SString;
 use tracing::{error, info, instrument};
@@ -9,12 +8,14 @@ use tracing::{error, info, instrument};
 /// Describes the administrative policy of a specific client
 pub(crate) struct ClientPolicy {
     pub key: Option<SString>,
+    pub groups: Option<Vec<String>>,
 }
 
 #[derive(Debug, Clone)]
 /// Describes the administrative policy of a specific user account
 pub(crate) struct UserPolicy {
     pub password: Option<SString>,
+    pub groups: Option<Vec<String>>,
 }
 
 #[derive(Debug, Clone, Default)]
@@ -36,75 +37,96 @@ pub(crate) fn load() -> Result<Policy, Box<dyn Error>>{
     let policy_file = policy_file.pop().unwrap();
     let root = policy_file.as_hash().unwrap();
     for (section, val) in root {
-        dbg!(section, val);
         match section.as_str().unwrap() {
-            "global" |
-            "clients" |
-            "users" |
-            "groups" => {}
+            "global" => parse_policy_global_section(&mut ret, val),
+            "clients" => parse_policy_clients_section(&mut ret, val),
+            "users" => parse_policy_users_section(&mut ret, val),
+            "groups" => parse_policy_groups_section(&mut ret, val),
             _ => { panic!() }
         }
     }
-    panic!();
-    /*
-    for (section, val) in policy_file {
-        match section.as_str() {
-            "global" => {
-                if let Some(gk) = val.get("default_key")
-                    && gk.is_str()
-                {
-                    ret.default_key = Some(SString(gk.as_str().unwrap().into()));
-                }
-                if let Some(allow_unconfig_client) = val.get("block_unconfigured_client")
-                    && allow_unconfig_client.is_bool()
-                {
-                    ret.allow_unconfigured = allow_unconfig_client.as_bool().unwrap();
-                }
-            },
-            "client" => {
-                if val.is_table() {
-                    for (maybe_ip, client_policy) in val.as_table().unwrap() {
-                        if let Ok(ip) = maybe_ip.parse::<IpAddr>() &&
-                            client_policy.is_table()
-                        {
-                            ret.clients.insert(ip, ClientPolicy { key: None} );
-                            if let Some(client_key) = client_policy.as_table().unwrap().get("key") &&
-                            client_key.is_str()
-                            {
-                                ret.clients.get_mut(&ip).unwrap().key = Some(SString(client_key.as_str().unwrap().into()));
-                            }
-                        }
-                        else {
-                            error!(outer_section = ?val, client = ?maybe_ip, client_config = ?client_policy,
-                                "Invalid client section in policy.");
-                            continue;
-                        }
+    info!(policy = ?ret, "Final Parsed Policy");
+    return Ok(ret);
+}
+
+#[instrument]
+fn parse_policy_global_section(policy: &mut Policy, section: &StrictYaml) {
+    let section = section.as_hash().unwrap();
+    for (setting, val) in section {
+        let setting = setting.as_str().unwrap();
+        let val = val.as_str().unwrap();
+        match setting {
+            "default-key" => {
+                policy.default_key = Some(SString(val.to_owned()));
+            }
+            "block-unconfigured" => {
+                policy.allow_unconfigured = val.eq_ignore_ascii_case("false");
+            }
+            _ => {
+                error!("unknown setting \"{setting}\" in global section");
+            }
+        }
+    }
+}
+
+#[instrument]
+fn parse_policy_clients_section(policy: &mut Policy, section: &StrictYaml) {
+    let section = section.as_hash().unwrap();
+    for (setting, val) in section {
+        let setting = setting.as_str().unwrap();
+        let val = val.as_hash().unwrap();
+        if let Ok(client_ip) = setting.parse::<IpAddr>() {
+            let mut client_policy = ClientPolicy { key: None, groups: None};
+            for (client_setting, client_val) in val {
+                let client_setting = client_setting.as_str().unwrap();
+                let client_val = client_val.as_str().unwrap();
+                match client_setting {
+                    "key" => {
+                        client_policy.key = Some(SString(client_val.to_owned()));
                     }
-                }
-                else { todo!() }
-            },
-            "user" => {
-                if val.is_table() {
-                    for (username, user_policy) in val.as_table().unwrap() {
-                        if user_policy.is_table() {
-                            ret.users.insert(username.clone(), UserPolicy { password: None });
-                            if let Some(user_pass) = user_policy.as_table().unwrap().get("password")
-                                && user_pass.is_str()
-                                {
-                                    ret.users.get_mut(username).unwrap().password = Some(SString(user_pass.as_str().unwrap().into()));
-                                }
-                        }
-                        else {
-                            error!(outer = ?val, user= ?username, policy = ?user_policy, "Failed to parse user section");
-                            continue;
-                        }
+                    "groups" => {
+                        client_policy.groups = Some(client_val.split_ascii_whitespace().map(str::to_ascii_lowercase).collect());
+                    }
+                    _ => {
+                        error!("Unknown client policy setting \"{client_setting}\"");
                     }
                 }
             }
-            _ => {},
+            policy.clients.insert(client_ip, client_policy);
+        }
+        else {
+            error!("Failed to parse client policy: \"{setting}\" not an IP Address");
         }
     }
-    info!(policy = ?ret, "Final Parsed Policy");
-    */
-    return Ok(ret);
+}
+
+#[instrument]
+fn parse_policy_users_section(policy: &mut Policy, section: &StrictYaml) {
+    let section = section.as_hash().unwrap();
+    for (username, user_settings) in section {
+        let username = username.as_str().unwrap();
+        let user_settings = user_settings.as_hash().unwrap();
+        for (setting, val) in user_settings {
+            let mut user_policy = UserPolicy { password: None, groups: None };
+            let setting = setting.as_str().unwrap();
+            let val = val.as_str().unwrap();
+            match setting {
+                "password" => {
+                    user_policy.password = Some(SString(val.to_owned()));
+                },
+                "groups" => {
+                    user_policy.groups = Some(val.split_ascii_whitespace().map(str::to_ascii_lowercase).collect());
+                },
+                _ => {
+                    error!("Unknwon user policy setting \"{setting}\"");
+                }
+            }
+            policy.users.insert(username.to_owned(), user_policy);
+        }
+    }
+}
+
+#[instrument]
+fn parse_policy_groups_section(_policy: &mut Policy, _section: &StrictYaml) {
+    error!("groups policy not implemented");
 }
