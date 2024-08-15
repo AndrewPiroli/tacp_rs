ef1a059f9f6d8cb74046b9924722f1343e0a0a5e
Andrew Piroli <drewpiroli@gmail.com>
2024-08-15 12:04:57 -0400

    core: make obfuscation PAD an iterator

--- tacp/src/obfuscation.rs
+++ tacp/src/obfuscation.rs
@@ -1,6 +1,5 @@
-use crate::PacketHeader;
-use alloc::vec::Vec;
-use md5::{Md5, Digest};
+use crate::{PacketHeader, PacketLength};
+use md5::{digest::core_api::CoreWrapper, Digest, Md5, Md5Core};
 
 /// The packet body can then be de-obfuscated by XORing it bytewise with a pseudo-random pad.
 /// 
@@ -16,47 +15,76 @@ use md5::{Md5, Digest};
 /// 
 /// MD5_1 = MD5{session_id, key, version, seq_no} MD5_2 = MD5{session_id, key, version, seq_no, MD5_1} .... MD5_n = MD5{session_id, key, version, seq_no, MD5_n-1}
 /// When a server detects that the secrets it has configured for the device do not match, it MUST return ERROR.
-/// 
 
-#[allow(clippy::zero_prefixed_literal, clippy::identity_op)]
-pub fn gen_psuedo_pad(header: PacketHeader, shared_secret: &[u8]) -> Vec<u8> {
-    let len = header.length as usize;
-    // Endianness !
-    let sesssion_id = &[
-        ((header.session_id & 0xff000000) >> 24) as u8,
-        ((header.session_id & 0x00ff0000) >> 16) as u8,
-        ((header.session_id & 0x0000ff00) >> 08) as u8,
-        ((header.session_id & 0x000000ff) >> 00) as u8,
-    ];
-    let ver_plus_seq = &[header.version, header.seq_no];
-
-    let mut pad: Vec<u8> = Vec::with_capacity(len + 16);
-
-    let mut base = Md5::new(); // rust-analyzer fail
-    base.update(sesssion_id);
-    base.update(shared_secret);
-    base.update(ver_plus_seq);
-    let base = base.finalize();
-
-    pad.extend(base.iter());
-
-    let mut prev = base;
-    while pad.len() < len {
-        let mut next = Md5::new();
-        next.update(sesssion_id);
-        next.update(shared_secret);
-        next.update(ver_plus_seq);
-        next.update(prev);
-        prev = next.finalize();
-        pad.extend(prev);
+pub struct TacacsMd5Pad<'a> {
+    session_id: [u8; 4],
+    ver_plus_seq: [u8; 2],
+    remaining: PacketLength,
+    shared_secret: &'a[u8],
+    md5_state: CoreWrapper<Md5Core>,
+    md5_buf: [u8; 16],
+    buf_ptr: u8,
+}
+impl<'a> TacacsMd5Pad<'a> {
+    #[allow(clippy::zero_prefixed_literal, clippy::identity_op)]
+    pub fn new(header: PacketHeader, shared_secret: &'a [u8]) -> Self {
+        let mut s = TacacsMd5Pad {
+            session_id: [0; 4],
+            ver_plus_seq: [0;2],
+            remaining: 0,
+            shared_secret,
+            md5_state: Md5::new(),
+            md5_buf: [0; 16],
+            buf_ptr: 16,
+        };
+        s.remaining = header.length;
+        // FIXME: big endian
+        s.session_id[0] = ((header.session_id & 0xff000000) >> 24) as u8;
+        s.session_id[1] = ((header.session_id & 0x00ff0000) >> 16) as u8;
+        s.session_id[2] = ((header.session_id & 0x0000ff00) >> 08) as u8;
+        s.session_id[3] = ((header.session_id & 0x000000ff) >> 00) as u8;
+        s.ver_plus_seq[0] = header.version;
+        s.ver_plus_seq[1] = header.seq_no;
+        s.md5_state.update(s.session_id.iter());
+        s.md5_state.update(shared_secret);
+        s.md5_state.update(s.ver_plus_seq.iter());
+        s.md5_state.finalize_into_reset((&mut s.md5_buf).into());
+        s
     }
+}
 
-    pad.truncate(len);
-    pad
+impl<'a> Iterator for TacacsMd5Pad<'a> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.remaining == 0 {
+            return None;
+        }
+        let mut p = self.buf_ptr as usize;
+        if p > 0 {
+            self.buf_ptr -= 1;
+            self.remaining -= 1;
+        }
+        else {
+            self.md5_state.update(self.session_id.iter());
+            self.md5_state.update(self.shared_secret);
+            self.md5_state.update(self.ver_plus_seq.iter());
+            self.md5_state.update(self.md5_buf.iter());
+            self.md5_state.finalize_into_reset((&mut self.md5_buf).into());
+            self.remaining -= 1;
+            self.buf_ptr = 15;
+            p = 16;
+        }
+        Some(unsafe {*self.md5_buf.get_unchecked(16-p)})
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.remaining as usize, Some(self.remaining as usize))
+    }
 }
 
 pub fn obfuscate_in_place(header: PacketHeader, shared_secret: &[u8], packet_body: &mut [u8]) {
-    for (idx, padbyte) in gen_psuedo_pad(header, shared_secret).iter().enumerate() {
-        packet_body[idx] ^= *padbyte;
-    }
+    core::iter::zip(packet_body.iter_mut(), TacacsMd5Pad::new(header, shared_secret))
+    .for_each(|(packet_byte, padbyte)|{
+        *packet_byte ^= padbyte;
+    });
 }
