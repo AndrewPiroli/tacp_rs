b48bc33a320b8fa05f63035ea657120ea43ecd03
Andrew Piroli <drewpiroli@gmail.com>
2024-12-22 16:09:31 -0500

    client: factor out reply logic

--- test_client/src/main.rs
+++ test_client/src/main.rs
@@ -104,145 +104,149 @@ fn main() {
                     util::hexdump(&recv_body);
                     break;
                 }
-                let recv_parsed = recv_parsed.unwrap();
-                match recv_parsed.status {
-                    // Terminate
-                    AuthenReplyStatus::PASS => {
-                        println!("Authentication passed");
-                        if let Some(msg) = recv_parsed.get_serv_msg() {
-                            let msg = String::from_utf8_lossy(msg);
-                            println!("Server Message: {msg}");
-                        }
-                        if let Some(data) = recv_parsed.get_data() {
-                            println!("Server Data: ");
-                            util::hexdump(data);
-                            println!();
-                        }
-                        expected_reply = NextPacket::None;
-                    },
-                    AuthenReplyStatus::FAIL => {
-                        println!("Authentication failed");
-                        if let Some(msg) = recv_parsed.get_serv_msg() {
-                            let msg = String::from_utf8_lossy(msg);
-                            println!("Server Message: {msg}");
-                        }
-                        if let Some(data) = recv_parsed.get_data() {
-                            println!("Server Data: ");
-                            util::hexdump(data);
-                            println!();
-                        }
-                        expected_reply = NextPacket::None;
-                    },
-                    AuthenReplyStatus::RESTART => {
-                        println!("Error: Server sent Authen Status RESTART");
-                        if let Some(msg) = recv_parsed.get_serv_msg() {
-                            let msg = String::from_utf8_lossy(msg);
-                            println!("Server Message: {msg}");
-                        }
-                        if let Some(data) = recv_parsed.get_data() {
-                            println!("Server Data: ");
-                            util::hexdump(data);
-                            println!();
-                        }
-                        expected_reply = NextPacket::None;
-                    },
-                    AuthenReplyStatus::ERROR => {
-                        println!("Error: Server sent Authen Status ERROR");
-                        if let Some(msg) = recv_parsed.get_serv_msg() {
-                            let msg = String::from_utf8_lossy(msg);
-                            println!("Server Message: {msg}");
-                        }
-                        if let Some(data) = recv_parsed.get_data() {
-                            println!("Server Data: ");
-                            util::hexdump(data);
-                            println!();
-                        }
-                        expected_reply = NextPacket::None;
-                    },
-                    // Reply
-                    AuthenReplyStatus::GETDATA => {
-                        if let Some(msg) = recv_parsed.get_serv_msg() {
-                            let msg = String::from_utf8_lossy(msg);
-                            println!("Server Message: {msg}");
-                        }
-                        if let Some(data) = recv_parsed.get_data() {
-                            println!("Server Data: ");
-                            util::hexdump(data);
-                            println!();
-                        }
-                        println!("Server requests \"data!\"");
-                        use tacp::REPLY_FLAG_NOECHO;
-                        let user_msg = util::prompt_user_input("Enter data for reply: ", recv_parsed.flags & 1 << REPLY_FLAG_NOECHO == 1);
-                        let mut reply_body = unsafe {
-                            AuthenContinuePacket::boxed_to_bytes(AuthenContinuePacket::new(0, user_msg.as_bytes(), blank.as_bytes()))
-                        };
-                        let reply_header = PacketHeader::new(Version::VersionDefault, PacketType::AUTHEN, seq_no, 0, session_id, reply_body.len() as u32);
-                        util::encrypt(&mut reply_body, SupportedEncryption::RfcMd5 { key: args.key.as_bytes(), header: reply_header });
-                        let pkt = util::alloc_pkt(reply_header, &reply_body);
-                        if let Err(e) = util::send_packet(&mut stream,&pkt) {
-                            println!("Error sending reply: {:?}", e);
-                            break;
-                        }
-                        seq_no += 1;
-                        //expected reply fall through
-                    },
-                    AuthenReplyStatus::GETUSER => {
-                        if let Some(msg) = recv_parsed.get_serv_msg() {
-                            let msg = String::from_utf8_lossy(msg);
-                            println!("Server Message: {msg}");
-                        }
-                        if let Some(data) = recv_parsed.get_data() {
-                            println!("Server Data: ");
-                            util::hexdump(data);
-                            println!();
-                        }
-                        println!("Server requests username!");
-                        let username = util::prompt_user_input("Enter username: ", false);
-                        let mut reply_body = unsafe {
-                            AuthenContinuePacket::boxed_to_bytes(AuthenContinuePacket::new(0, username.as_bytes(), blank.as_bytes()))
-                        };
-                        let reply_header = PacketHeader::new(Version::VersionDefault, PacketType::AUTHEN, seq_no, 0, session_id, reply_body.len() as u32);
-                        util::encrypt(&mut reply_body, SupportedEncryption::RfcMd5 { key: args.key.as_bytes(), header: reply_header });
-                        let pkt = util::alloc_pkt(reply_header, &reply_body);
-                        if let Err(e) = util::send_packet(&mut stream,&pkt) {
-                            println!("Error sending reply: {:?}", e);
-                            break;
-                        }
-                        seq_no += 1;
-                        //expected reply fall through
-                    },
-                    AuthenReplyStatus::GETPASS => {
-                        if let Some(msg) = recv_parsed.get_serv_msg() {
-                            let msg = String::from_utf8_lossy(msg);
-                            println!("Server Message: {msg}");
-                        }
-                        if let Some(data) = recv_parsed.get_data() {
-                            println!("Server Data: ");
-                            util::hexdump(data);
-                            println!();
-                        }
-                        println!("Server requests password!");
-                        let pass = util::prompt_user_input("Enter password: ", true);
-                        let mut reply_body = unsafe {
-                            AuthenContinuePacket::boxed_to_bytes(AuthenContinuePacket::new(0, pass.as_bytes(), blank.as_bytes()))
-                        };
-                        let reply_header = PacketHeader::new(Version::VersionDefault, PacketType::AUTHEN, seq_no, 0, session_id, reply_body.len() as u32);
-                        util::encrypt(&mut reply_body, SupportedEncryption::RfcMd5 { key: args.key.as_bytes(), header: reply_header });
-                        let pkt = util::alloc_pkt(reply_header, &reply_body);
-                        if let Err(e) = util::send_packet(&mut stream,&pkt) {
-                            println!("Error sending reply: {:?}", e);
-                            break;
-                        }
-                        seq_no += 1;
-                        //expected reply fall through
-                    },
-                    AuthenReplyStatus::FOLLOW => {
-                        println!("Authen Follow not implemented");
-                        break;
-                    },
-                }
+                handle_authen_reply(recv_parsed.unwrap(), &mut expected_reply, &mut stream, &mut seq_no, session_id, args.key.as_bytes());
             },
             NextPacket::None => unreachable!(),
         }
+    }
+}
+
+fn handle_authen_reply(packet: &AuthenReplyPacket, next_packet: &mut NextPacket, stream: &mut TcpStream, seq_no: &mut u8, session_id: u32, key: &[u8]) {
+    let blank = "";
+    match packet.status {
+        // Terminate
+        AuthenReplyStatus::PASS => {
+            println!("Authentication passed");
+            if let Some(msg) = packet.get_serv_msg() {
+                let msg = String::from_utf8_lossy(msg);
+                println!("Server Message: {msg}");
+            }
+            if let Some(data) = packet.get_data() {
+                println!("Server Data: ");
+                util::hexdump(data);
+                println!();
+            }
+            *next_packet = NextPacket::None;
+        },
+        AuthenReplyStatus::FAIL => {
+            println!("Authentication failed");
+            if let Some(msg) = packet.get_serv_msg() {
+                let msg = String::from_utf8_lossy(msg);
+                println!("Server Message: {msg}");
+            }
+            if let Some(data) = packet.get_data() {
+                println!("Server Data: ");
+                util::hexdump(data);
+                println!();
+            }
+            *next_packet = NextPacket::None;
+        },
+        AuthenReplyStatus::RESTART => {
+            println!("Error: Server sent Authen Status RESTART");
+            if let Some(msg) = packet.get_serv_msg() {
+                let msg = String::from_utf8_lossy(msg);
+                println!("Server Message: {msg}");
+            }
+            if let Some(data) = packet.get_data() {
+                println!("Server Data: ");
+                util::hexdump(data);
+                println!();
+            }
+            *next_packet = NextPacket::None;
+        },
+        AuthenReplyStatus::ERROR => {
+            println!("Error: Server sent Authen Status ERROR");
+            if let Some(msg) = packet.get_serv_msg() {
+                let msg = String::from_utf8_lossy(msg);
+                println!("Server Message: {msg}");
+            }
+            if let Some(data) = packet.get_data() {
+                println!("Server Data: ");
+                util::hexdump(data);
+                println!();
+            }
+            *next_packet = NextPacket::None;
+        },
+        // Reply
+        AuthenReplyStatus::GETDATA => {
+            if let Some(msg) = packet.get_serv_msg() {
+                let msg = String::from_utf8_lossy(msg);
+                println!("Server Message: {msg}");
+            }
+            if let Some(data) = packet.get_data() {
+                println!("Server Data: ");
+                util::hexdump(data);
+                println!();
+            }
+            println!("Server requests \"data!\"");
+            use tacp::REPLY_FLAG_NOECHO;
+            let user_msg = util::prompt_user_input("Enter data for reply: ", packet.flags & 1 << REPLY_FLAG_NOECHO == 1);
+            let mut reply_body = unsafe {
+                AuthenContinuePacket::boxed_to_bytes(AuthenContinuePacket::new(0, user_msg.as_bytes(), blank.as_bytes()))
+            };
+            let reply_header = PacketHeader::new(Version::VersionDefault, PacketType::AUTHEN, *seq_no, 0, session_id, reply_body.len() as u32);
+            util::encrypt(&mut reply_body, SupportedEncryption::RfcMd5 { key: key, header: reply_header });
+            let pkt = util::alloc_pkt(reply_header, &reply_body);
+            if let Err(e) = util::send_packet(stream,&pkt) {
+                println!("Error sending reply: {:?}", e);
+                return;
+            }
+            *seq_no += 1;
+            //expected reply fall through
+        },
+        AuthenReplyStatus::GETUSER => {
+            if let Some(msg) = packet.get_serv_msg() {
+                let msg = String::from_utf8_lossy(msg);
+                println!("Server Message: {msg}");
+            }
+            if let Some(data) = packet.get_data() {
+                println!("Server Data: ");
+                util::hexdump(data);
+                println!();
+            }
+            println!("Server requests username!");
+            let username = util::prompt_user_input("Enter username: ", false);
+            let mut reply_body = unsafe {
+                AuthenContinuePacket::boxed_to_bytes(AuthenContinuePacket::new(0, username.as_bytes(), blank.as_bytes()))
+            };
+            let reply_header = PacketHeader::new(Version::VersionDefault, PacketType::AUTHEN, *seq_no, 0, session_id, reply_body.len() as u32);
+            util::encrypt(&mut reply_body, SupportedEncryption::RfcMd5 { key: key, header: reply_header });
+            let pkt = util::alloc_pkt(reply_header, &reply_body);
+            if let Err(e) = util::send_packet(stream,&pkt) {
+                println!("Error sending reply: {:?}", e);
+                return;
+            }
+            *seq_no += 1;
+            //expected reply fall through
+        },
+        AuthenReplyStatus::GETPASS => {
+            if let Some(msg) = packet.get_serv_msg() {
+                let msg = String::from_utf8_lossy(msg);
+                println!("Server Message: {msg}");
+            }
+            if let Some(data) = packet.get_data() {
+                println!("Server Data: ");
+                util::hexdump(data);
+                println!();
+            }
+            println!("Server requests password!");
+            let pass = util::prompt_user_input("Enter password: ", true);
+            let mut reply_body = unsafe {
+                AuthenContinuePacket::boxed_to_bytes(AuthenContinuePacket::new(0, pass.as_bytes(), blank.as_bytes()))
+            };
+            let reply_header = PacketHeader::new(Version::VersionDefault, PacketType::AUTHEN, *seq_no, 0, session_id, reply_body.len() as u32);
+            util::encrypt(&mut reply_body, SupportedEncryption::RfcMd5 { key: key, header: reply_header });
+            let pkt = util::alloc_pkt(reply_header, &reply_body);
+            if let Err(e) = util::send_packet(stream,&pkt) {
+                println!("Error sending reply: {:?}", e);
+                return;
+            }
+            *seq_no += 1;
+            //expected reply fall through
+        },
+        AuthenReplyStatus::FOLLOW => {
+            println!("Authen Follow not implemented");
+            return;
+        },
     }
 }
